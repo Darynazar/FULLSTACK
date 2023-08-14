c4375d6dca7a9e19ab97275b5201cc857af459d5
Darynazar <heydari.std00@gmail.com>
2023-08-14 18:21:37 +0330

    add api and html css in welcome.blade and css/app.css

--- resources/js/router/index.js
+++ resources/js/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 
-import invoiceIndex from '../components/invoices/index.vue'
+import invoiceIndex from '../components/invoices/index.vue';
+
 import notFound from '../components/invoices/NotFound.vue';
 
 
